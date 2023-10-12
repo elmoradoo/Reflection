edfa294d145a397df30e63550d67929b65f5cf96
mourad.elsadwy <mourad.elsadwy@FR-LTP-0202>
2023-10-12 16:20:33 +0200

    State machine WIP

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,48 @@
 extends CharacterBody3D
 
 #Nodes
-
-@onready var head = $head
+@onready var player = $"."
+@onready var head = $neck/head
 @onready var standing_collision_shape = $standing_collision_shape
 @onready var crouching_collision_shape = $crouching_collision_shape
 @onready var top_of_head = $raycasts/top_of_head
+@onready var neck = $neck
 
 #Config vars
 const mouse_sensitivity: float = 0.2
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+#Player states
+
+enum player_states {
+	Idle,
+	Walking,
+	Sprinting,
+	Jumping,
+	Crouching,
+	Sliding
+}
+
+var current_state : player_states = player_states.Idle
 #Player stats
 const crouching_speed: float = 3.0
 const walking_speed: float = 5.0
 const sprinting_speed: float = 8.0
 const jump_velocity: float = 4.5
 const sliding_minimum_velocity: float = 7.0
-
+const sliding_initial_force: float = 9.0
+const sliding_friction: float = 4.0
 #Mouvement vars
 var current_speed: float = 5.0
-var crouching_depth: float = 0.5
+var crouching_depth: float = -0.5
 var lerp_speed: float = 10.0
 var direction: Vector3 = Vector3.ZERO
 
 #Player state
 var is_running: bool = false
 var is_sprinting: bool = false
-var is_crouching: bool = false
 var is_sliding: bool = false
+var free_looking: bool = false
 
 var input_dir
 func _ready():
@@ -36,48 +50,96 @@ func _ready():
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		if free_looking:
+			neck.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		else:
+			rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
-func running():
+func sprinting():
 	is_running = true
 	current_speed = sprinting_speed
 
 func crouching(delta):
-	is_crouching = true
 	current_speed = crouching_speed
 	standing_collision_shape.disabled = true
 	crouching_collision_shape.disabled = false
 	head.position.y = lerp(head.position.y, crouching_depth, lerp_speed * delta)
 	
-func sliding():
-	pass
+func sliding(delta):
+	is_sliding = true
+	free_looking = true
+	standing_collision_shape.disabled = true
+	crouching_collision_shape.disabled = false
+	head.position.y = lerp(head.position.y, crouching_depth, lerp_speed * delta)
+	var slide_force = direction.normalized() * sliding_initial_force
+	velocity.x = slide_force.x
+	velocity.z = slide_force.z
+	velocity.x = move_toward(velocity.x, 0, sliding_friction * delta)
+	velocity.z = move_toward(velocity.z, 0, sliding_friction * delta)
 
 func walking():
 	current_speed = walking_speed
 
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	if Input.is_action_pressed("crouch") and is_on_floor():
-		crouching(delta)
-	elif not top_of_head.is_colliding():
-		head.position.y = lerp(head.position.y, 1.8, lerp_speed * delta)
+func jumping():
+	velocity.y = jump_velocity
+	
+func inputs_checks(delta):
+	input_dir = Input.get_vector("left", "right", "forward", "backward")
+	if is_on_floor() and Input.is_action_pressed("crouch") and velocity.length() >= sliding_minimum_velocity:
+		current_state = player_states.Sliding
+	elif is_on_floor() and Input.is_action_pressed("crouch"):
+		current_state = player_states.Crouching
+	elif is_on_floor() and Input.is_action_just_released("crouch") and not top_of_head.is_colliding():
+		free_looking = false
+		current_state = player_states.Idle
+		head.position.y = 0.0
+		#head.position.y = lerp(head.position.y, 0.0, lerp_speed * delta)
 		standing_collision_shape.disabled = false
 		crouching_collision_shape.disabled = true
-		if Input.is_action_pressed("sprint") and is_on_floor():
-			running()
-		else:
+	elif Input.is_action_pressed("sprint"):
+		current_state = player_states.Sprinting
+	else:
+		current_state = player_states.Walking
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		current_state = player_states.Jumping
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+
+func _physics_process(delta):
+	inputs_checks(delta)
+
+	match current_state:
+		player_states.Idle:
+			pass
+		player_states.Walking:
 			walking()
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-	input_dir = Input.get_vector("left", "right", "forward", "backward")
+		player_states.Sprinting:
+			sprinting()
+		player_states.Jumping:
+			jumping()
+		player_states.Crouching:
+			crouching(delta)
+		player_states.Sliding:
+			sliding(delta)
+			
 	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), lerp_speed * delta)
+	
 	if direction:
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
+		if is_sliding:
+			pass
+		else:
+			velocity.x = direction.x * current_speed
+			velocity.z = direction.z * current_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
+	if is_sliding and velocity.length() < 1.0:
+		is_sliding = false
+	current_state = player_states.Idle
 	move_and_slide()
+	
+func _process(_delta):
+	pass
