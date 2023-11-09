4881ff2a9c10f74b5864746f2aa83968d7034fa5
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2023-11-09 22:52:16 +0100

    LevelSpawner and PlayerSpawner are now in the multiplayer scene

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -6,11 +6,6 @@ func _ready():
 	$VBoxContainer/new_game_button.pressed.connect(self.new_game)
 	$VBoxContainer/multiplayer_button.pressed.connect(self.multi)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
 func new_game():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	get_tree().root.add_child(load("res://scenes/player.tscn").instantiate())
--- scripts/multiplayer/multiplayer.gd
+++ scripts/multiplayer/multiplayer.gd
@@ -1,23 +1,10 @@
 # multiplayer.gd
 extends Node
 
-const PORT = 7000
+const PORT = 25565
 
 func _ready():
-	if not multiplayer.is_server():
-		return
-
-	# Start paused.
-	#get_tree().paused = true
-	# You can save bandwidth by disabling server relay and peer notifications.
 	multiplayer.server_relay = false
-
-	# Automatically start the server in headless mode.
-	if DisplayServer.get_name() == "headless":
-		print("Automatically starting dedicated server.")
-		_on_host_pressed.call_deferred()
-
-	multiplayer.peer_connected.connect(spawn_player)
 
 func _on_host_pressed():
 	# Start as server.
@@ -50,49 +37,40 @@ func _input(event):
 	if not multiplayer.is_server():
 		return
 	if event.is_action("ui_home") and Input.is_action_just_pressed("ui_home"):
-		change_level(load("res://scenes/world.tscn"))
-		spawn_peer_players()
-		if not OS.has_feature("dedicated_server"):
-			spawn_player(1)
-
-func remove_children(node: Node):
-	for child in node.get_children():
-		node.remove_child(child)
-		child.queue_free()
+		change_level.call_deferred(load("res://scenes/world.tscn"))
+		spawn_all_players.call_deferred()
 
 func change_level(scene: PackedScene):
-	# Remove old level if any
-	var spawner = $LevelSpawner
-	remove_children(spawner)
+	var level = scene.instantiate()
+	# Remove previous level
+	for child in $Level.get_children():
+		$Level.remove_child(child)
+		child.queue_free()
 	# Load new level
-	spawner.add_child(scene.instantiate())
+	$Level.add_child(level)
 
-func spawn_peer_players():
+func spawn_all_players():
+	# Remove previous players, if any
+	for child in $Players.get_children():
+		$Players.remove_child(child)
+		child.queue_free()
+
 	for player_id in multiplayer.get_peers():
 		spawn_player(player_id)
+	spawn_player(multiplayer.multiplayer_peer.get_unique_id())
 
 func spawn_player(id: int):
-	var loaded_level = $LevelSpawner.get_child(0)
-	if not loaded_level:
-		print_debug("Level not yet loaded.")
-		return
-	for child in loaded_level.get_children():
-		if child.name == str(id):
-			print_debug("Player " + str(id) + " already spawned.")
-			return
-
 	var player = load("res://scenes/player.tscn").instantiate()
 	player.name = str(id)
-	loaded_level.add_child(player)
-	print("Spawned player " + str(id))
+	$Players.add_child(player)
+	print("Spawned player " + str(id) + str(multiplayer.is_server()))
 
 func start_game():
 	# Hide the UI and unpause to start the game.
 	$UI.hide()
-	get_tree().paused = false
 	# Only change level on the server.
 	# Clients will instantiate the level via the spawner.
 	if multiplayer.is_server():
-		change_level(load("res://scenes/world.tscn"))
-		if not OS.has_feature("dedicated_server"):
-			spawn_player(1)
+		multiplayer.peer_connected.connect(spawn_player)
+		change_level.call_deferred(load("res://scenes/world.tscn"))
+		spawn_all_players.call_deferred()
