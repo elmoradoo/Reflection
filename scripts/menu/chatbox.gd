f5d253dba9f781401e22792c1d206f3f5decd6e8
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2023-11-10 14:05:55 +0100

    chatbox: make bg transparent and add colors to chat

--- scripts/menu/chatbox.gd
+++ scripts/menu/chatbox.gd
@@ -1,9 +1,18 @@
 extends Node
 
 
+var HISTORY_FOCUS_TRANSPARENCY: float = 1
+var HISTORY_UNFOCUS_TRANSPARENCY: float = 0
+var SEND_FOCUS_TRANSPARENCY: float = 1
+var SEND_UNFOCUS_TRANSPARENCY: float = 0.5
+
+func _ready():
+	$Container/ChatHistory/Panel.modulate.a = HISTORY_UNFOCUS_TRANSPARENCY
+	$Container/SendMessage.modulate.a = SEND_UNFOCUS_TRANSPARENCY
+
 @rpc("any_peer", "call_local", "unreliable")
 func new_chat_message(user: String, message: String):
-	$Container/ChatHistory.text += user + ": " + message
+	$Container/ChatHistory.text += "[b][color=red]" + user + "[/color][/b]" + ": " + "[color=black]" + message + "[/color]"
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_text_newline"):
@@ -14,3 +23,11 @@ func _physics_process(_delta):
 			$Container/SendMessage.release_focus()
 		else:
 			$Container/SendMessage.grab_focus()
+
+func _on_send_message_focus_entered():
+	$Container/ChatHistory/Panel.modulate.a = HISTORY_FOCUS_TRANSPARENCY
+	$Container/SendMessage.modulate.a = SEND_FOCUS_TRANSPARENCY
+
+func _on_send_message_focus_exited():
+	$Container/ChatHistory/Panel.modulate.a = HISTORY_UNFOCUS_TRANSPARENCY
+	$Container/SendMessage.modulate.a = SEND_UNFOCUS_TRANSPARENCY
