aca58c0d0e7c2c3790685e0c660247c2551c9ddc
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2023-11-12 16:27:47 +0100

    Workaround look_at failed error

--- scripts/player/debug/debug_velocity.gd
+++ scripts/player/debug/debug_velocity.gd
@@ -3,4 +3,10 @@ extends RayCast3D
 
 func _on_player_line_update(origin, end, length):
 	target_position = Vector3(0, 0, -length)
+
+	# Workaround the dumb "Error look_at() failed"
+	# because when falling, end is equal to Vector3.UP
+	end.x += 0.01
+	end.z += 0.01
+
 	look_at(end)
