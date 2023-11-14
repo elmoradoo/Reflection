cb4d1de7a1832e7f9b14da63872821721d894da1
Mourad <Mourad@DESKTOP-8EQ0CLJ>
2023-11-14 15:01:48 +0100

    jumping have forward velocity now & airtime boost if fall from medium height

--- scripts/player/states/airtime.gd
+++ scripts/player/states/airtime.gd
@@ -3,7 +3,6 @@ extends BaseState
 
 const max_speed: float = 3.0
 var velocity_before_landing: Vector3
-
 const roll_min_velocity = -8
 
 func get_state_name():
@@ -27,7 +26,12 @@ func get_input_next_state():
 		return enums.player_states.Rolling
 
 func get_physics_next_state():
-	if player.is_on_floor():
+	if player.is_on_floor() and velocity_before_landing.y < -5:
+		var forward = player.transform.basis.z.normalized()
+		player.position.x = lerp(player.position.x, player.position.x + (-forward.x * 4.0), 2.0 * player.delta)
+		player.position.z = lerp(player.position.z, player.position.z + (-forward.z * 4.0), 2.0 * player.delta)
+		return enums.player_states.Sprinting
+	elif player.is_on_floor():
 		return enums.player_states.Idle
 	elif super.can_ledge_grab():
 		return enums.player_states.LedgeGrab
--- scripts/player/states/jumping.gd
+++ scripts/player/states/jumping.gd
@@ -2,7 +2,7 @@ extends BaseState
 
 
 const jump_velocity: float = 4.5
-
+const forward_velocity: float = 1.0
 
 func get_state_name():
 	return enums.player_states.Jumping
@@ -12,10 +12,11 @@ func enter():
 	player.model.get_node("AnimationPlayer").play("jumping")
 
 func move_player():
-	#Set eyes to 0
-	player.eyes.position.x = lerp(player.eyes.position.x, 0.0, player.delta * 1)
-	player.eyes.position.y = lerp(player.eyes.position.x, 0.0, player.delta * 1)
+	super.reset_neck(2.0)
 	player.velocity.y += jump_velocity
+	var forward = player.transform.basis.z.normalized()
+	player.position.x = lerp(player.position.x, player.position.x + (-forward.x * 4.0), forward_velocity * player.delta)
+	player.position.z = lerp(player.position.z, player.position.z + (-forward.z * 4.0), forward_velocity * player.delta)
 	super.move_player()
 
 
