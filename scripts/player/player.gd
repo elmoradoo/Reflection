7e54bc2c5740ddf9e7142c5f6202e23c55dbec93
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2023-12-23 00:33:06 +0100

    Moved raycasts from idle to player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -46,6 +46,33 @@ var target_rotation: float = 0
 var is_rotating: bool = false
 var rotation_lerp: float = 0
 
+var raycasts_left: Array[RayCast3D] = []
+var raycasts_right: Array[RayCast3D] = []
+
+func init_raycasts(num_raycasts):
+	for i in range(num_raycasts):
+		var raycast_left: RayCast3D = RayCast3D.new()
+		var raycast_right: RayCast3D = RayCast3D.new()
+
+		raycast_left.position.x = 0.5
+		raycast_left.target_position = Vector3(0, 0, -1)
+		raycast_left.enabled = true
+
+		raycast_right.position.x = -0.5
+		raycast_right.target_position = Vector3(0, 0, -1)
+		raycast_right.enabled = true
+
+		var height_offset: float = i * 0.1
+		raycast_left.position.y = height_offset
+		raycast_right.position.y = height_offset
+
+		raycasts_left.append(raycast_left)
+		raycasts_right.append(raycast_right)
+
+		self.add_child(raycast_left)
+		self.add_child(raycast_right)
+
+
 func _enter_tree():
 	if str(name).is_valid_int():
 		set_multiplayer_authority(str(name).to_int())
@@ -93,6 +120,9 @@ func _ready():
 	hud_timer.timeout.connect(self._on_get_stats_timeout)
 	add_child(hud_timer)
 	hud_timer.start(0.1)
+
+	# Raycasts
+	self.init_raycasts(40)
 
 
 # This is only used for mouse events.
--- scripts/player/states/idle.gd
+++ scripts/player/states/idle.gd
@@ -2,37 +2,8 @@ extends State
 
 @export var idle_lerp_speed: float = 2.0 
 
-var raycasts_left: Array[RayCast3D] = []
-var raycasts_right: Array[RayCast3D] = []
-
 func init(player_obj: Player):
 	super.init(player_obj)
-	# Number of raycasts
-	var num_raycasts: int = 40
-	# Create and position raycasts
-	for i in range(num_raycasts):
-		var raycast_left: RayCast3D = RayCast3D.new()
-		var raycast_right: RayCast3D = RayCast3D.new()
-	# Set raycast properties
-		raycast_left.position.x = 0.5
-		raycast_left.target_position = Vector3(0, 0, -1)  # Adjust as needed
-		raycast_left.enabled = true
-
-		raycast_right.position.x = -0.5		
-		raycast_right.target_position = Vector3(0, 0, -1)  # Adjust as needed
-		raycast_right.enabled = true
-		
-		# Position raycasts above the previous one
-		var height_offset: float = i * 0.1
-		raycast_left.position.y = height_offset
-		raycast_right.position.y = height_offset
-		
-		# Append the fully configured raycast to the array
-		raycasts_left.append(raycast_left)
-		raycasts_right.append(raycast_right)
-		# Add the raycast as a child
-		player.add_child(raycast_left)
-		player.add_child(raycast_right)
 
 
 func can_enter(_prev_state: String):
